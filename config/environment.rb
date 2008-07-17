56963a86078ea619944e3186961e945a17fa80df
karthik.cs <karthik.cs@gmail.com>
2008-07-16 20:23:19 -0700

    added campfire trademark sentence removed campfire logo moved common view code to layout

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,3 @@
-RAILS_GEM_VERSION = '2.0.2'
 # Be sure to restart your server when you modify this file
 
 # Uncomment below to force Rails into production mode when
@@ -6,6 +5,7 @@ RAILS_GEM_VERSION = '2.0.2'
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
+#RAILS_GEM_VERSION = '2.1.0' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -19,7 +19,6 @@ Rails::Initializer.run do |config|
   # Skip frameworks you're not going to use (only works if using vendor/rails).
   # To use Rails without a database, you must remove the Active Record framework
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
-  config.frameworks -= [ :active_record ]
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
