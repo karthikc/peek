adf405b12649675d9248ac07076572ef5826b9dc
karthik c <karthik.cs@gmail.com>
2008-10-01 20:11:20 +0530

    using hoptoad to manage exceptions

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,7 +1,9 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 
+
 class ApplicationController < ActionController::Base
+  include HoptoadNotifier::Catcher
   helper :all # include all helpers, all the time
 
   # See ActionController::RequestForgeryProtection for details
---
+++ config/initializers/hoptoad.rb
@@ -0,0 +1,3 @@
+HoptoadNotifier.configure do |config|
+  config.api_key = '8ac885b5c431e9e5cde9be3a6c7a04a9'
+end
